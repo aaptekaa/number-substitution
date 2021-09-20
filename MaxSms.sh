a44c899eada10144812e5d5d59eee8144520c2ff
KaliSecurityMax <70202505+KaliSecurityMax@users.noreply.github.com>
2021-09-20 07:22:17 +0300

    Add files via upload

---
+++ MaxSms.sh
@@ -0,0 +1,5 @@
+curl -X POST https://textbelt.com/text \
+       --data-urlencode phone='НОМЕР ДЛЯ ОТПРАВКИ' \
+       --data-urlencode senderid='ОТПРАВИТЕЛЬ' \
+       --data-urlencode message='СООБЩЕНИЕ' \
+       -d key=textbelt
